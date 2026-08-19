fmma/opends-fork

test: add multithreaded sync-read coverage
Four threads start behind a barrier and hammer opends_read on a shared
handle with per-thread buffers, verifying every read against the
in-memory pattern. The ref variant runs on the host (clean under
ThreadSanitizer); the aisio variant exercises concurrent slot claims
and P2P reads on the HOMI/qublk stack.

Signed-off-by: Frederik Madsen <f.madsen@samsung.com>
Assisted-by: Claude:claude-fable-5

scripts/run_tests.py
@@ -16,9 +16,9 @@ from _helpers import ok, run_cijoe
 
 BACKENDS = ["ref", "gds", "aisio"]
 TESTS_BY_BACKEND = {
-    "ref":   ["smoke", "sync_read"],
+    "ref":   ["smoke", "sync_read", "mt_read"],
     "gds":   ["sync_read"],
-    "aisio": ["sync_read", "register_large", "async_read",
+    "aisio": ["sync_read", "mt_read", "register_large", "async_read",
               "sync_write", "async_write", "block_alloc"],
 }
 ALL_TESTS = sorted({t for ts in TESTS_BY_BACKEND.values() for t in ts})

xnvme/opends

Assisted-by: Claude Code:claude-fable-5

scripts/run_tests.py
@@ -16,9 +16,9 @@ from _helpers import ok, run_cijoe
 
 BACKENDS = ["ref", "gds", "aisio"]
 TESTS_BY_BACKEND = {
-    "ref":   ["smoke", "sync_read"],
+    "ref":   ["smoke", "sync_read", "mt_read"],
     "gds":   ["sync_read"],
-    "aisio": ["sync_read", "register_large", "coherency",
+    "aisio": ["sync_read", "mt_read", "register_large", "coherency",
               "async_read", "sync_write", "async_write", "block_alloc"],
 }
 ALL_TESTS = sorted({t for ts in TESTS_BY_BACKEND.values() for t in ts})